8a652f5ce01326a040901acdbabbe26f2a7a8661
Yuri Buyanov <yuri.buyanov@gmail.com>
2021-09-14 21:39:08 +0300

    Initial commit

---
+++ app/src/main/java/stangenzirkel/mathmultitool/MainActivity.java
@@ -0,0 +1,41 @@
+package stangenzirkel.mathmultitool;
+
+import android.os.Bundle;
+
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    NavController navController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        setSupportActionBar(findViewById(R.id.toolbar));
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, drawer);
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, new AppBarConfiguration.Builder(
+                R.id.calculatorFragment, R.id.converterFragment)
+                .setDrawerLayout(findViewById(R.id.drawer_layout))
+                .build())
+                || super.onSupportNavigateUp();
+    }
+}
